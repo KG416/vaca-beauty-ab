284d6a0f1d99fd3273527246696d9ecfe33c59e3
karlG <kallegunnarsson@hotmail.com>
2022-05-17 13:19:58 +0200

    Better contrast on "show more" button

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,26 +1,11 @@
 // style
 import styles from './ProductList.module.scss'
 
-// libs
-import { useState, useEffect } from 'react'
-
 // components
-import allProducts from '../../data/products.json'
 import ProductItem from '../ProductItem'
-import Button from '../Button'
 
-const ProductList = () => {
-  const [currentProducts, setCurrentProducts] = useState([])
-  const [showAllProducts, setShowAllProducts] = useState(false)
-  const MIN_NO_OF_PRODUCTS = 4
-
-  useEffect(() => {
-    if (showAllProducts) return setCurrentProducts(allProducts)
-    setCurrentProducts(allProducts.slice(0, MIN_NO_OF_PRODUCTS))
-  }, [showAllProducts])
-
+const ProductList = ({ currentProducts = [] }) => {
   return (
-    <>
       <ul className={styles.wrapper}>
         {currentProducts &&
           currentProducts.map((product) => (
@@ -34,11 +19,6 @@ const ProductList = () => {
             />
           ))}
       </ul>
-
-        <Button variant="secondary" onClick={() => setShowAllProducts((prevState) => !prevState)}>
-          {currentProducts.length === MIN_NO_OF_PRODUCTS ? 'Show more' : 'Show less'}
-        </Button>
-    </>
   )
 }
 
--- src/views/Products/index.jsx
+++ src/views/Products/index.jsx
@@ -1,18 +1,42 @@
-import ProductList from '../../components/ProductList'
-import Button from '../../components/Button'
+// style
 import styles from './Products.module.scss'
 
+// libs
+import { useState, useEffect } from 'react'
+
+// data
+import allProducts from '../../data/products.json'
+
+// components
+import ProductList from '../../components/ProductList'
+import Button from '../../components/Button'
+
 const Poducts = () => {
+  const [currentProducts, setCurrentProducts] = useState([])
+  const [showAllProducts, setShowAllProducts] = useState(false)
+  const MIN_NO_OF_PRODUCTS = 4
+
+  useEffect(() => {
+    if (showAllProducts) return setCurrentProducts(allProducts)
+    setCurrentProducts(allProducts.slice(0, MIN_NO_OF_PRODUCTS))
+  }, [showAllProducts])
+
   return (
     <div className={styles.wrapper}>
       <h2>View products</h2>
 
-      <div className={styles.buttonWrapper}>
+      <div className={styles.btnWrapperTop}>
         <Button>Price</Button>
         <Button>New</Button>
       </div>
 
-      <ProductList />
+      <ProductList currentProducts={currentProducts} />
+
+      <div className={styles.btnWrapperBottom}>
+        <Button variant="secondary" onClick={() => setShowAllProducts((prevState) => !prevState)}>
+          {currentProducts.length === MIN_NO_OF_PRODUCTS ? 'Show more' : 'Show less'}
+        </Button>
+      </div>
     </div>
   )
 }
