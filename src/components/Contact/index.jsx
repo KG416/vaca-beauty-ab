04c1e6754bb97fe7cff0e9889cd2625849d30abe
karlG <kallegunnarsson@hotmail.com>
2022-05-17 10:34:14 +0200

    all images in public folder

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,7 +3,10 @@ import Button from '../Button'
 
 const Contact = () => {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} style={{
+      // eslint-disable-next-line no-undef
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.3)), url(${process.env.PUBLIC_URL + '/assets/image-6.jpg'})`
+    }}>
       <h2>Get in touch</h2>
       <p>47 Chandos Place, London, WC2N 4HS</p>
       <Button variant="secondary">Contact us</Button>
